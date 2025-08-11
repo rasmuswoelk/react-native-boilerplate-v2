2b11e6f84338797053f671ea74e7f50b7acf2bb0
Rasmus Wølk <rasmuswoelk@gmail.com>
2025-08-11 14:58:14 +0200

    dx: fixed ts errors

--- lib/theme/types.ts
+++
@@ -1,8 +0,0 @@
-import { BaseTheme } from "./theme";
-
-// Global theme type that will be augmented by the app
-declare global {
-  namespace Theme {
-    interface AppTheme extends BaseTheme {}
-  }
-}
