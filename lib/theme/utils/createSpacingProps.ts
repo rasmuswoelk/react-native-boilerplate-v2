b110032bb184164f95ea450ee9eeaf65ac7c51bf
Rasmus Wølk <rasmuswoelk@gmail.com>
2026-05-27 15:56:14 +0200

    refactor: use useUnistyles in createSpacingProps

--- lib/theme/utils/createSpacingProps.ts
+++ lib/theme/utils/createSpacingProps.ts
@@ -1,83 +1,80 @@
-import omit from "lodash/omit";
-import pick from "lodash/pick";
-import { useMemo } from "react";
-import { ViewStyle } from "react-native";
-import { useTheme } from "../hooks/useTheme";
-import { SpacingKeyType, SpacingType } from "../variables/spacing";
+import omit from 'lodash/omit'
+import pick from 'lodash/pick'
+import { useMemo } from 'react'
+import { ViewStyle } from 'react-native'
+import { useUnistyles } from 'react-native-unistyles'
+import { SpacingKeyType, SpacingType } from '../variables/spacing'
 
-// Define all possible spacing style properties
 export const spacingStyleProperties = [
-  "margin",
-  "marginTop",
-  "marginRight",
-  "marginBottom",
-  "marginLeft",
-  "marginHorizontal",
-  "marginVertical",
-  "padding",
-  "paddingTop",
-  "paddingRight",
-  "paddingBottom",
-  "paddingLeft",
-  "paddingHorizontal",
-  "paddingVertical",
-];
+  'margin',
+  'marginTop',
+  'marginRight',
+  'marginBottom',
+  'marginLeft',
+  'marginHorizontal',
+  'marginVertical',
+  'padding',
+  'paddingTop',
+  'paddingRight',
+  'paddingBottom',
+  'paddingLeft',
+  'paddingHorizontal',
+  'paddingVertical',
+]
 
-type SpacingStyleProperty = (typeof spacingStyleProperties)[number];
+type SpacingStyleProperty = (typeof spacingStyleProperties)[number]
 
-// Create spacing props type that allows all spacing properties with spacing keys as values
 export type SpacingProps = {
-  margin?: SpacingKeyType;
-  marginTop?: SpacingKeyType;
-  marginRight?: SpacingKeyType;
-  marginBottom?: SpacingKeyType;
-  marginLeft?: SpacingKeyType;
-  marginHorizontal?: SpacingKeyType;
-  marginVertical?: SpacingKeyType;
-  padding?: SpacingKeyType;
-  paddingTop?: SpacingKeyType;
-  paddingRight?: SpacingKeyType;
-  paddingBottom?: SpacingKeyType;
-  paddingLeft?: SpacingKeyType;
-  paddingHorizontal?: SpacingKeyType;
-  paddingVertical?: SpacingKeyType;
-};
+  margin?: SpacingKeyType
+  marginTop?: SpacingKeyType
+  marginRight?: SpacingKeyType
+  marginBottom?: SpacingKeyType
+  marginLeft?: SpacingKeyType
+  marginHorizontal?: SpacingKeyType
+  marginVertical?: SpacingKeyType
+  padding?: SpacingKeyType
+  paddingTop?: SpacingKeyType
+  paddingRight?: SpacingKeyType
+  paddingBottom?: SpacingKeyType
+  paddingLeft?: SpacingKeyType
+  paddingHorizontal?: SpacingKeyType
+  paddingVertical?: SpacingKeyType
+}
 
-// Utility function to create spacing styles from spacing props
 export const createSpacingStyles = (
   props: SpacingProps,
   spacing: SpacingType
 ): Partial<ViewStyle> => {
-  const styles: Partial<ViewStyle> = {};
+  const styles: Partial<ViewStyle> = {}
 
   Object.entries(props).forEach(([property, spacingKey]) => {
     if (spacingKey && spacingKey in spacing) {
-      const styleProperty = property as SpacingStyleProperty;
-      (styles as any)[styleProperty] = spacing[spacingKey];
+      const styleProperty = property as SpacingStyleProperty
+      ;(styles as any)[styleProperty] = spacing[spacingKey]
     }
-  });
+  })
 
-  return styles;
-};
+  return styles
+}
 
 export const getSpacingPropertiesByComponentProps = (
   props: Record<string, unknown>
 ): SpacingProps => {
-  return pick(props, spacingStyleProperties) as SpacingProps;
-};
+  return pick(props, spacingStyleProperties) as SpacingProps
+}
 
 export const omitSpacingProps = (
   props: Record<string, unknown>
 ): Record<string, unknown> => {
-  return omit(props, spacingStyleProperties);
-};
+  return omit(props, spacingStyleProperties)
+}
 
 export const useGetSpacingStylesByComponentProps = (
   props: Record<string, unknown>
 ): Partial<ViewStyle> => {
-  const { theme } = useTheme();
+  const { theme } = useUnistyles()
   return useMemo(() => {
-    const spacingProps = getSpacingPropertiesByComponentProps(props);
-    return createSpacingStyles(spacingProps, theme.spacing);
-  }, [props, theme.spacing]);
-};
+    const spacingProps = getSpacingPropertiesByComponentProps(props)
+    return createSpacingStyles(spacingProps, theme.spacing)
+  }, [props, theme.spacing])
+}
