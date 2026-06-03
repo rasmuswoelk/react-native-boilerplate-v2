9988ed0bfb4a9d7b78ddac9fb711a19cee3e4c49
Rasmus Wølk <rasmuswoelk@gmail.com>
2026-06-03 22:52:34 +0200

    minor: updated the default initial screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
-import { useWindowDimensions, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Container } from '@/lib/components/Container';
-import { FadeIn } from '@/lib/components/FadeIn';
-import { AnimatedGradient } from '@/lib/components/Gradient';
-import { LottieView } from '@/lib/components/LottieView';
-import { Typography } from '@/lib/components/Typography';
-import animation from '@/src/assets/animations/3d-shape-animation.json';
+import { useWindowDimensions, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Container } from "@/lib/components/Container";
+import { FadeIn } from "@/lib/components/FadeIn";
+import { AnimatedGradient } from "@/lib/components/Gradient";
+import { LottieView } from "@/lib/components/LottieView";
+import { Typography } from "@/lib/components/Typography";
+import animation from "@/src/assets/animations/3d-shape-animation.json";
 
 export const GradientBackground = () => {
   const { width, height } = useWindowDimensions();
@@ -14,8 +14,8 @@ export const GradientBackground = () => {
     <AnimatedGradient
       width={width}
       height={height}
-      style={{ flex: 1, position: 'absolute', inset: 0, zIndex: -1 }}
-      colors={['#ff1c73', '#4b69a2']}
+      style={{ flex: 1, position: "absolute", inset: 0, zIndex: -1 }}
+      colors={["#0e1a4e", "#08142a"]}
       direction="random"
       animationType="shift"
       speed="slow"
@@ -29,13 +29,13 @@ const Animation = () => {
   return (
     <View
       style={{
-        height: '100%',
-        position: 'absolute',
+        height: "100%",
+        position: "absolute",
         inset: 0,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        overflow: 'hidden',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        overflow: "hidden",
       }}
     >
       <FadeIn delay={100} duration={800}>
@@ -53,19 +53,29 @@ const Animation = () => {
 export default function Index() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, zIndex: 10, position: 'relative' }}>
+      <SafeAreaView style={{ flex: 1, zIndex: 10, position: "relative" }}>
         <Container
           gutter="lg"
           paddingTop="3xl"
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
-          <FadeIn delay={1200} translateY={-20} duration={650} style={{ width: '100%' }}>
-            <Typography color="white" fontSize="5xl" lineHeight={1.2} marginBottom="sm">
-              <Typography fontWeight="black" color="white">
-                Welcome to{'\n'}
+          <FadeIn
+            delay={1200}
+            translateY={-20}
+            duration={650}
+            style={{ width: "100%" }}
+          >
+            <Typography
+              color="white"
+              fontSize="5xl"
+              lineHeight={1.2}
+              marginBottom="sm"
+            >
+              <Typography fontWeight="regular" color="white">
+                Let's start{"\n"}
               </Typography>
-              <Typography fontWeight="regular" color="white" fontStyle="italic">
-                the boilerplate
+              <Typography fontWeight="black" color="white" fontStyle="italic">
+                building.
               </Typography>
             </Typography>
           </FadeIn>
